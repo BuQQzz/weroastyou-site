4d4ed8af9c2be5cced39d7590f3e6750f90434df
BuQQzz <buqqzz@gmail.com>
2026-04-01 19:40:35 -0400

    Initial deploy - We Roast You

---
+++ server.js
@@ -0,0 +1,14 @@
+﻿const http = require('http');
+const fs = require('fs');
+const path = require('path');
+const server = http.createServer((req, res) => {
+  const filePath = path.join('C:\\Users\\Frances\\.openclaw\\agents\\brook\\agent\\website', req.url === '/' ? 'index.html' : req.url);
+  const ext = path.extname(filePath);
+  const types = {'.html':'text/html','.css':'text/css','.js':'text/javascript','.png':'image/png'};
+  fs.readFile(filePath, (err, data) => {
+    if (err) { res.writeHead(404); res.end('Not found'); return; }
+    res.writeHead(200, {'Content-Type': types[ext] || 'text/plain'});
+    res.end(data);
+  });
+});
+server.listen(8765, () => console.log('Server running on port 8765'));
